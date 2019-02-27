961ec6e6c472c8047321d91c35b2ad5b2d4f3609
stalary <452024236@qq.com>
2019-02-27 10:19:10 +0800

    ADD:104

---
+++ 104.go
@@ -0,0 +1,19 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	// 计算左节点深度
+	left := maxDepth(root.Left)
+	// 计算右节点深度
+	right := maxDepth(root.Right)
+	// 将左右两边较大值相加
+	return int(math.Max(float64(left), float64(right))) + 1
+}

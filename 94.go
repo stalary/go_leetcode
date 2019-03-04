e490cc0c3a81ccb76c7d4b91721f1f45a451dfe0
stalary <452024236@qq.com>
2019-03-04 11:34:42 +0800

    ADD:94

---
+++ 94.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"stalary/study"
+)
+
+func main() {
+	right := &study.TreeNode{
+		Left: &study.TreeNode{
+			Left:  nil,
+			Right: nil,
+			Val:   3,
+		},
+		Right: nil,
+		Val:   2,
+	}
+	root := &study.TreeNode{
+		Left:  nil,
+		Right: right,
+		Val:   1,
+	}
+	fmt.Println(inorderTraversal(root))
+}
+
+func inorderTraversal(root *study.TreeNode) []int {
+	ret := make([]int, 0)
+	if root == nil {
+		return ret
+	}
+	stack := list.New()
+	for root != nil || stack.Len() != 0 {
+		for root != nil {
+			fmt.Println(root.Val)
+			stack.PushBack(root)
+			root = root.Left
+		}
+		root = stack.Back().Value.(*study.TreeNode)
+		ret = append(ret, root.Val)
+		stack.Remove(stack.Back())
+		root = root.Right
+	}
+	return ret
+}

f1d0579743d0ba616c32132d88f1fb093e3ea3c0
stalary <452024236@qq.com>
2019-03-04 13:02:32 +0800

    ADD:104

--- 104.go
+++ 104.go
@@ -1,20 +1,23 @@
 package main
 
-import "math"
+import (
+	"math"
+	"stalary/study"
+)
 
 func main() {
 
 }
 
-// 计算二叉树的最大深度
-func maxDepth(root *TreeNode) int {
+// calculate tree depth
+func maxDepth(root *study.TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	// 计算左节点深度
+	// left depth
 	left := maxDepth(root.Left)
-	// 计算右节点深度
+	// right depth
 	right := maxDepth(root.Right)
-	// 将左右两边较大值相加
+	// sum max(left, right)
 	return int(math.Max(float64(left), float64(right))) + 1
 }

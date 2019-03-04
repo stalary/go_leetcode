b1f0fbc121108b4311141481965ba391ab3c1d02
stalary <452024236@qq.com>
2019-03-04 12:57:48 +0800

    ADD:461

--- 461.go
+++ 461.go
@@ -9,11 +9,10 @@ func main() {
 	fmt.Println(hammingDistance(1, 4))
 }
 
-// 求两个数的hamming距离
+// calculate hamming distance
 func hammingDistance(x int, y int) int {
-	// 异或操作
 	or := x ^ y
-	// 获取总共有多少个1
+	// count one
 	count := bits.OnesCount(uint(or))
 	return count
 }

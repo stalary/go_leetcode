707fed6e8d6759df695409344366128b8d7d308f
stalary <452024236@qq.com>
2019-03-04 12:52:28 +0800

    ADD:328

---
+++ 328.go
@@ -0,0 +1,24 @@
+package main
+
+import "stalary/study"
+
+func oddEvenList(head *study.ListNode) *study.ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// odd list
+	odd := head
+	// even head list, use it append to odd list
+	evenHead := head.Next
+	// even list
+	even := evenHead
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		even.Next = even.Next.Next
+		odd = odd.Next
+		even = even.Next
+	}
+	// append even head list
+	odd.Next = evenHead
+	return head
+}
